db9dcc19df5bf3c5d22fc6ff6becb01a78a6f169
xueliang cao <2357747972@qq.com>
2021-08-31 17:18:22 +0800

    Object.defignProperty基本使用

--- Vue源码探秘之数据响应式原理/Vue_data_ reactive/webpack.config.js
+++ Vue源码探秘之数据响应式原理/Vue_data_ reactive/webpack.config.js
@@ -1,6 +1,38 @@
 const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { DefinePlugin } = require("webpack");
 
 module.exports = {
+  mode: "development",
+  devtool: "source-map",
   entry: "./src/index.js",
-  output: {},
+  output: {
+    filename: "bundle.js",
+    path: resolve(__dirname, "build"),
+  },
+  devServer: {
+    open: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "",
+      template: "./public/index.html",
+    }),
+    new DefinePlugin({
+      BASE_URL: '"./"',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public",
+          globOptions: {
+            ignore: ["**/index.html", "**/DS_Store"],
+          },
+        },
+      ],
+    }),
+  ],
 };
