980beabd95b96ed1096ef9868ca13b8eb945a0d0
CaoXueLiang <2357747972@qq.com>
2022-03-17 14:21:21 +0800

    解析器

--- vue-2.6.14/src/compiler/index.js
+++ vue-2.6.14/src/compiler/index.js
@@ -1,25 +1,32 @@
 /* @flow */
 
-import { parse } from './parser/index'
-import { optimize } from './optimizer'
-import { generate } from './codegen/index'
-import { createCompilerCreator } from './create-compiler'
+import { parse } from "./parser/index";
+import { optimize } from "./optimizer";
+import { generate } from "./codegen/index";
+import { createCompilerCreator } from "./create-compiler";
 
-// `createCompilerCreator` allows creating compilers that use alternative
-// parser/optimizer/codegen, e.g the SSR optimizing compiler.
-// Here we just export a default compiler using the default parts.
-export const createCompiler = createCompilerCreator(function baseCompile (
+/**
+ * 1. 将模板解析成AST抽象语法树
+ * 2. 遍历AST，标记静态节点和静态根节点
+ * 3. 将 AST生成渲染函数
+ */
+export const createCompiler = createCompilerCreator(function baseCompile(
   template: string,
   options: CompilerOptions
 ): CompiledResult {
-  const ast = parse(template.trim(), options)
+  const ast = parse(template.trim(), options);
+  // 优化，遍历 AST，为每个节点做静态标记
+  // 标记每个节点是否为静态节点，然后进一步标记出静态根节点
+  // 这样在后续更新中就可以跳过这些静态节点了
+  // 标记静态根，用于生成渲染函数阶段，生成静态根节点的渲染函数
   if (options.optimize !== false) {
-    optimize(ast, options)
+    optimize(ast, options);
   }
-  const code = generate(ast, options)
+  // 从 AST 生成渲染函数，生成像这样的代码，比如：code.render = "_c('div',{attrs:{"id":"app"}},_l((arr),function(item){return _c('div',{key:item},[_v(_s(item))])}),0)"
+  const code = generate(ast, options);
   return {
     ast,
     render: code.render,
-    staticRenderFns: code.staticRenderFns
-  }
-})
+    staticRenderFns: code.staticRenderFns,
+  };
+});
--- vue-2.6.14/src/platforms/web/entry-runtime-with-compiler.js
+++ vue-2.6.14/src/platforms/web/entry-runtime-with-compiler.js
@@ -1,101 +1,127 @@
 /* @flow */
 
-import config from 'core/config'
-import { warn, cached } from 'core/util/index'
-import { mark, measure } from 'core/util/perf'
+import config from "core/config";
+import { warn, cached } from "core/util/index";
+import { mark, measure } from "core/util/perf";
 
-import Vue from './runtime/index'
-import { query } from './util/index'
-import { compileToFunctions } from './compiler/index'
-import { shouldDecodeNewlines, shouldDecodeNewlinesForHref } from './util/compat'
+import Vue from "./runtime/index";
+import { query } from "./util/index";
+import { compileToFunctions } from "./compiler/index";
+import {
+  shouldDecodeNewlines,
+  shouldDecodeNewlinesForHref,
+} from "./util/compat";
 
-const idToTemplate = cached(id => {
-  const el = query(id)
-  return el && el.innerHTML
-})
+const idToTemplate = cached((id) => {
+  const el = query(id);
+  return el && el.innerHTML;
+});
 
-const mount = Vue.prototype.$mount
+const mount = Vue.prototype.$mount;
+/**
+ * 编译器的入口
+ * 就做了一件事，得到组件的渲染函数，将其设置到`this.$options`上
+ */
 Vue.prototype.$mount = function (
   el?: string | Element,
   hydrating?: boolean
 ): Component {
-  el = el && query(el)
+  // 挂载点
+  el = el && query(el);
 
-  /* istanbul ignore if */
+  // 挂载点不能是 body 或者 html，只能挂载到 normal elements上
   if (el === document.body || el === document.documentElement) {
-    process.env.NODE_ENV !== 'production' && warn(
-      `Do not mount Vue to <html> or <body> - mount to normal elements instead.`
-    )
-    return this
+    process.env.NODE_ENV !== "production" &&
+      warn(
+        `Do not mount Vue to <html> or <body> - mount to normal elements instead.`
+      );
+    return this;
   }
 
-  const options = this.$options
+  // 配置项
+  const options = this.$options;
   // resolve template/el and convert to render function
+  // 如果用户提供了 render 配置项，则直接跳过编译阶段，否则进入编译阶段
+  // 解析 template和el,并转换为 render 函数
+  // 优先级：render > template > el
+
   if (!options.render) {
-    let template = options.template
+    let template = options.template;
     if (template) {
-      if (typeof template === 'string') {
-        if (template.charAt(0) === '#') {
-          template = idToTemplate(template)
+      if (typeof template === "string") {
+        // 处理 template 选项
+        if (template.charAt(0) === "#") {
+          // { template: '#app' }，template 是一个 id 选择器，则获取该元素的 innerHtml 作为模板
+          template = idToTemplate(template);
           /* istanbul ignore if */
-          if (process.env.NODE_ENV !== 'production' && !template) {
+          if (process.env.NODE_ENV !== "production" && !template) {
             warn(
               `Template element not found or is empty: ${options.template}`,
               this
-            )
+            );
           }
         }
       } else if (template.nodeType) {
-        template = template.innerHTML
+        // template 是一个正常的元素，获取其 innerHtml 作为模版
+        template = template.innerHTML;
       } else {
-        if (process.env.NODE_ENV !== 'production') {
-          warn('invalid template option:' + template, this)
+        if (process.env.NODE_ENV !== "production") {
+          warn("invalid template option:" + template, this);
         }
-        return this
+        return this;
       }
     } else if (el) {
-      template = getOuterHTML(el)
+      // 设置了 el 选项，获取 el 选择器的 outerHtml 作为模版
+      template = getOuterHTML(el);
     }
     if (template) {
+      // 模板就绪，进入编译阶段
       /* istanbul ignore if */
-      if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-        mark('compile')
+      if (process.env.NODE_ENV !== "production" && config.performance && mark) {
+        mark("compile");
       }
 
-      const { render, staticRenderFns } = compileToFunctions(template, {
-        outputSourceRange: process.env.NODE_ENV !== 'production',
-        shouldDecodeNewlines,
-        shouldDecodeNewlinesForHref,
-        delimiters: options.delimiters,
-        comments: options.comments
-      }, this)
-      options.render = render
-      options.staticRenderFns = staticRenderFns
+      // 编译模板得到渲染函数
+      const { render, staticRenderFns } = compileToFunctions(
+        template,
+        {
+          outputSourceRange: process.env.NODE_ENV !== "production",
+          shouldDecodeNewlines,
+          shouldDecodeNewlinesForHref,
+          delimiters: options.delimiters,
+          comments: options.comments,
+        },
+        this
+      );
+      // 将两个渲染函数放到 this.$options
+      options.render = render;
+      options.staticRenderFns = staticRenderFns;
 
       /* istanbul ignore if */
-      if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-        mark('compile end')
-        measure(`vue ${this._name} compile`, 'compile', 'compile end')
+      if (process.env.NODE_ENV !== "production" && config.performance && mark) {
+        mark("compile end");
+        measure(`vue ${this._name} compile`, "compile", "compile end");
       }
     }
   }
-  return mount.call(this, el, hydrating)
-}
+  // 进行挂载
+  return mount.call(this, el, hydrating);
+};
 
 /**
  * Get outerHTML of elements, taking care
  * of SVG elements in IE as well.
  */
-function getOuterHTML (el: Element): string {
+function getOuterHTML(el: Element): string {
   if (el.outerHTML) {
-    return el.outerHTML
+    return el.outerHTML;
   } else {
-    const container = document.createElement('div')
-    container.appendChild(el.cloneNode(true))
-    return container.innerHTML
+    const container = document.createElement("div");
+    container.appendChild(el.cloneNode(true));
+    return container.innerHTML;
   }
 }
 
-Vue.compile = compileToFunctions
+Vue.compile = compileToFunctions;
 
-export default Vue
+export default Vue;
